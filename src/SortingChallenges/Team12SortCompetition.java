c724316f187809ba9fad80b31d172fe095117172
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-12-04 12:25:51 -0500

    yes

---
+++ src/SortingChallenges/Team12SortCompetition.java
@@ -0,0 +1,34 @@
+package SortingChallenges;
+
+public class Team12SortCompetition 
+{
+	public static void main(String[] args) 
+	{
+
+	}
+	
+	public static double challengeOne(int[] dataSet)
+	{
+		return 1;
+	}
+	
+	public static int challengeTwo(String[] dataSet)
+	{
+		return -1;
+	}
+	
+	public static double challengeThree(int[] dataSet)
+	{
+		return 1;
+	}
+	
+	public static double challengeFour(int[][] dataSet)
+	{
+		return 1;
+	}
+	
+	public static int challengeFive()
+	{
+		return -1;
+	}
+}
